5a88f91aa7705312efa5545a87f373a165b61498
AP-Porag <smauhp@gmail.com>
2024-09-05 01:27:28 +0600

    committed

---
+++ app/home.jsx
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native'
+import React from 'react'
+
+const Home = () => {
+    return (
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text>Home</Text>
+            <Text>Purpose: Purpose of this Home screen.</Text>
+        </View>
+    )
+}
+
+export default Home
